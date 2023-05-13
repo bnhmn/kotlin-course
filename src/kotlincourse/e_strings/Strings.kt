45d94ef713e5945b99d45f5c3a7f4302d4d3db99
Benjamin Heimann <benjamin.heimann@outlook.de>
2023-05-13 22:59:16 +0200

    Add Strings.kt

---
+++ src/kotlincourse/e_strings/Strings.kt
@@ -0,0 +1,20 @@
+package kotlincourse.e_strings
+
+fun main() {
+    // String Templates (String interpolation)
+    val template = "String Template"
+    println("This is a $template")
+    println("This is not a \$template")
+    println("1 + 2 = ${1 + 2}")
+
+    // Raw Strings
+    val filePathA = "C:\\Users\benja\\Desktop"
+    val filePathB = """C:\Users\benja\Desktop"""
+    val multiline = """
+        <p>
+        This is still a $template.
+        1 + 2 = ${1 + 2}
+        </p>
+    """.trimIndent()
+    println(multiline)
+}
