5f5d22c3960f0bfb0ec7d117f4c4bc3dfcab74f4
Benjamin Heimann <benjamin.heimann@outlook.de>
2023-05-13 23:07:52 +0200

    Add Repl.kt

---
+++ src/kotlincourse/f_repl/Repl.kt
@@ -0,0 +1,13 @@
+package kotlincourse.f_repl
+
+/*
+IntelliJ can run a Read Eval Print Loop (REPL) for Kotlin.
+
+It is located under Tools -> Kotlin -> Kotlin REPL.
+
+It allows you to interactively execute Kotlin code
+like if Kotlin was a scripting language like Python.
+Useful to quickly test some code.
+
+Enter statements into the REPL terminal and Press Ctrl-Enter to execute.
+ */
