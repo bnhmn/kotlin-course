1427d773abd5badcd8fc052890884abf7e564218
Benjamin Heimann <benjamin.heimann@outlook.de>
2023-06-05 13:58:44 +0200

    Add ClassInheritance.kt

---
+++ src/kotlincourse/l_inheritance/ClassInheritance.kt
@@ -0,0 +1,49 @@
+package kotlincourse.l_inheritance
+
+import kotlin.math.PI
+import kotlin.math.pow
+
+/*
+You have to declare a class as 'open' or 'abstract', else you cannot extend it!
+
+class Shape             ✖
+data class Shape        ✖   Data classes can extend other classes, but cannot be extended themselves
+
+open class Shape        ✔
+abstract class Shape    ✔
+interface Shape         ✔
+ */
+
+
+abstract class Shape {
+    abstract fun area(): Double
+
+    open fun print() {  // You have to explicitly declare a method 'open' or 'abstract', else you cannot override it!
+        println("Area: ${this.area()} m²")
+    }
+}
+
+class Square(val a: Number) : Shape() {                     // Must call super constructor
+    override fun area(): Double = a.toDouble().pow(2)       // Must use 'override' keyword when overriding functions
+    override fun print() = println("Square(a=$a): ${this.area()} m²")
+}
+
+class Circle(val r: Number) : Shape() {
+    override fun area(): Double = PI * r.toDouble().pow(2)
+    override fun print() = println("Circle(r=$r): ${this.area()} m²")
+}
+
+class Triangle(val g: Number, val hg: Number) : Shape() {
+    override fun area(): Double = g.toDouble() * hg.toDouble() / 2
+    override fun print() = println("Triangle(g=$g, hg=$hg): ${this.area()} m²")
+}
+
+
+fun main() {
+    val shapes = listOf(
+        Square(2),
+        Circle(1),
+        Triangle(1, 2)
+    )
+    shapes.forEach { it.print() }
+}
---
+++ src/kotlincourse/l_inheritance/ClassInheritanceMulti.kt
@@ -0,0 +1,24 @@
+package kotlincourse.l_inheritance
+
+// Multi level class inheritance
+
+abstract class AShape {
+    abstract fun area(): Double
+    abstract fun print()
+}
+
+open class ARectangle(val a: Number, val b: Number) : AShape() {    // Class must be declared 'open' to be extendable!
+    override fun area(): Double = a.toDouble() * b.toDouble()
+    override fun print() = println("ARectangle(a=$a, b=$b): ${this.area()} m²")
+}
+
+open class ASquare(a: Number) : ARectangle(a, a)
+
+
+fun main() {
+    val shapes = listOf(
+        ARectangle(1, 2),
+        ASquare(2),
+    )
+    shapes.forEach { it.print() }
+}
